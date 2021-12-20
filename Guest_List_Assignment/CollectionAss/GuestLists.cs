ff48554b7ff2c9c1976e6562bea5cfecd360c3c1
Bernard Tieku <76092596+tiekunak@users.noreply.github.com>
2021-12-19 20:23:42 -0600

    Add files via upload

---
+++ Guest_List_Assignment/CollectionAss/GuestLists.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionAss
+{
+    class GuestLists
+    {
+        public static void first()
+        {
+            Console.WriteLine("wlecome to the GuestList");
+            
+            List<string> guestsList = new List<string>();
+            
+            bool flag = true;
+            do
+            {
+
+                
+                Console.WriteLine("Enter the name of guest");
+                string name = (Console.ReadLine());
+                guestsList.Add(name);
+
+
+                Console.WriteLine("Do you want to enter another name?");
+                Console.WriteLine("y or n");
+                string yes = Console.ReadLine();
+
+                if (yes.ToLower() == "y")
+                {
+                    
+                }
+                else
+                {
+                    flag = false;
+                }
+               
+            } while (flag);
+         
+            foreach (string guest in guestsList)
+            {
+                Console.WriteLine(guest);
+            }
+
+
+        }
+
+        public static void second()
+        {
+            Console.WriteLine("wlecome to the GuestList");
+
+            ArrayList gueList = new ArrayList();
+
+            bool flag = true;
+            do
+            {
+
+
+                Console.WriteLine("Enter the name of guest");
+                string name = (Console.ReadLine());
+                gueList.Add(name);
+
+
+                Console.WriteLine("Do you want to enter another name?");
+                Console.WriteLine("y or n");
+                string yes = Console.ReadLine();
+
+                if (yes.ToLower() == "y")
+                {
+
+                }
+                else
+                {
+                    flag = false;
+                }
+
+            } while (flag);
+
+            foreach (string gues in gueList)
+            {
+                Console.WriteLine(gues);
+            }
+
+
+        }
+
+
+        public static void third()
+        {
+            Console.WriteLine("wlecome to the GuestList");
+
+            ArrayList gueList = new ArrayList();
+
+            bool flag = true;
+            do
+            {
+
+
+                Console.WriteLine("Enter the name of guest");
+                string name = (Console.ReadLine());
+                gueList.Add(name);
+
+
+                Console.WriteLine("Do you want to enter another name?");
+                Console.WriteLine("y or n");
+                string yes = Console.ReadLine();
+
+                if (yes.ToLower() == "y")
+                {
+
+                }
+                else
+                {
+                    flag = false;
+                }
+
+            } while (flag);
+
+            foreach (string gues in gueList)
+            {
+                Console.WriteLine(gues);
+            }
+        }
+
+    }
+
+    class FunGame
+    {
+        public static void fouth()
+        {
+            Console.WriteLine("wlecome to the Drawing Game");
+            Console.WriteLine("Choose between number 3, 5 and 7");
+            int bet = int.Parse(Console.ReadLine());
+
+            if (bet == 3)
+            {
+                Console.WriteLine("***");
+            }
+            else if(bet == 5)
+            {
+                Console.WriteLine("*****");
+            }
+            else
+            {
+                Console.WriteLine("*******");
+            }
+        }
+    }
+}
---
+++ Guest_List_Assignment/CollectionAss/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionAss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool flag = true;
+            do
+            {
+                Console.WriteLine("Choose an Option");
+                Console.WriteLine("1: List Guest List");
+                Console.WriteLine("2: Array Guest List");
+                Console.WriteLine("3: Array Guest List");
+                Console.WriteLine("4: To exist");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        GuestLists.first();
+                        break;
+                    case 2:
+                        GuestLists.second();
+                        break;
+                    case 3:
+                        FunGame.fouth();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("There is no such program");
+                        break;
+
+                }
+            } while (flag);
+        }
+
+    }
+}
