1a32320909b6ca366162b0fb40d336486e699f01
Bernard Tieku <76092596+tiekunak@users.noreply.github.com>
2021-12-19 20:50:52 -0600

    Add files via upload

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.light();
+
+            Truck myTruck = new Truck();
+            myTruck.light();
+
+            Console.WriteLine(myCar.brand + " " + myCar.modelName + " with " + myCar.noOfDoor + " doors ");
+            Console.WriteLine(myTruck.brand + " " + myTruck.modelName2 + " with " + myTruck.noOfDoors + " doors ");
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Vehicles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Vehicles
+    {
+        public string brand = "Opel";
+
+        public void light()
+        {
+            string wl = "white Light";
+            string nl = "Normal Light";
+            Console.WriteLine(wl + " and " + nl);
+        }
+    }
+
+    class Car : Vehicles
+    {
+        public string modelName = "Astra";
+        public int noOfDoor = 4;
+        public string year = "2008";
+    }
+
+    class Truck : Vehicles
+    {
+        public string modelName2 = "Maultier";
+        public int noOfDoors = 2;
+        public string year = "1980";
+    }
+}
